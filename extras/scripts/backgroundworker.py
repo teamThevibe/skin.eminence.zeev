df09db776ef6f9371a1ed75c1cfd74a52376eceb
teamThevibe <team.thevibe@gmail.com>
2016-04-01 18:27:47 +0300

    scripts update - log file , startup

--- extras/scripts/backgroundworker.py
+++ extras/scripts/backgroundworker.py
@@ -17,9 +17,12 @@ import urlparse
 import zipfile
 import sqlite3
 import json
+import uuid
 
 MetaSettingsUrl = 'http://www.the-vibe.co.il/Wizard/wtf/?category=addonsettings&Id=102';
 QuasarSettingsUrl = 'http://www.the-vibe.co.il/Wizard/wtf/?category=addonsettings&Id=33'
+SaltsSettingsUrl = 'http://www.the-vibe.co.il/Wizard/wtf/?category=addonsettings&Id=34'
+
 Package_Definition_Url = "http://www.the-vibe.co.il/Skin/GetSkinBackupPath";
 BASEURL = 'http://www.the-vibe.co.il'
 APIurl = 'http://www.the-vibe.co.il/api'
@@ -36,8 +39,19 @@ dialog= xbmcgui.Dialog()
 ENABLE_PVR_COMMAND='delete from disabled where addonID="pvr.iptvsimple"'
 metasettingsPath = xbmc.translatePath(os.path.join('special://home','userdata','addon_data','plugin.video.meta','players'))
 quasarsettingsPath = xbmc.translatePath(os.path.join('special://home','userdata','addon_data','plugin.video.quasar','settings.xml'))
+saltssettingsPath = xbmc.translatePath(os.path.join('special://home','userdata','addon_data','plugin.video.salts','settings.xml'))
 USERDATA     =  xbmc.translatePath(os.path.join('special://home/userdata',''))
+UUID_FILE_PATH = xbmc.translatePath(os.path.join('special://skin/extras','scripts','uniq.txt'))
 
+def getUniqId():
+    try:
+        if os.path.exists(UUID_FILE_PATH):
+            return open(UUID_FILE_PATH,'r').read()
+        else:
+            open(UUID_FILE_PATH,'w').write(str(uuid.uuid4()))
+            return open(UUID_FILE_PATH,'r').read()
+    except:
+        pass
 
 def find_str(s, char):
     index = 0
@@ -119,8 +133,9 @@ def wizard(name,url):
     lib = os.path.join(path, name)
     try:
        os.remove(lib)
-    except:
-       pass
+    except Exception as e:
+        xbmc.log(str(e))
+        pass
     download(url, lib, dp)
 
 def clear():
@@ -179,17 +194,6 @@ def getAddonDbName():
 		return 'Addons19.db'
 ADDON_DB = xbmc.translatePath(os.path.join(USERDATA,'Database',getAddonDbName()))
 
-def enablePvr():
-    try:
-        conn = sqlite3.connect(ADDON_DB)
-        conn.execute(ENABLE_PVR_COMMAND)
-        conn.commit()
-        conn.close()
-        xbmc.executebuiltin("UpdateLocalAddons");xbmc.executebuiltin("UpdateAddonRepos")
-        xbmcgui.Dialog().ok('הפעלת לקוח טלויזיה חיה','לקוח טלויזיה חיה הופעל בהצלחה','יש לאתחל את הקודי על מנת שהשינוי יכנס לתוקף')
-    except:
-        pass
-
 def GetMetaSettings(name,url):
         path = xbmc.translatePath(os.path.join('special://home','addons','packages'))
         dp = xbmcgui.DialogProgress() 
@@ -218,6 +222,19 @@ def GetQuasarSettings(name,url):
         xbmc.executebuiltin("UpdateLocalAddons")
         xbmc.executebuiltin("UpdateAddonRepos")
 
+def GetSaltsSettings(name,url):
+        path = xbmc.translatePath(os.path.join('special://home','addons','packages'))
+        dp = xbmcgui.DialogProgress() 
+        dp.create("הפעלה ראשונה:","מתקין הגדרת נגנים עבור הרחבת  Salts ",'','רץ בפתיחה ראשונה בלבד') 
+        lib = os.path.join(path,name + '.zip')
+        try: os.remove(lib)
+        except: pass
+        downloadMetaSettings(url,lib,dp)
+        time.sleep(2)
+        #dp.update(0,"","Installing selections.....")
+        all(lib,xbmc.translatePath('special://home'),'')
+        xbmc.executebuiltin("UpdateLocalAddons")
+        xbmc.executebuiltin("UpdateAddonRepos")
 
 def installMetaSettings():
     if not os.path.exists(metasettingsPath):
@@ -227,10 +244,20 @@ def installQuasarSettings():
     if not os.path.exists(quasarsettingsPath):
             GetQuasarSettings('QuasarSettings',QuasarSettingsUrl)
 
+def installSaltsSettings():
+    if not os.path.exists(saltssettingsPath):
+            GetSaltsSettings('SaltsSettings',SaltsSettingsUrl)
 
-
-#
-
+def getUniqId():
+    try:
+        if os.path.exists(UUID_FILE_PATH):
+            return open(UUID_FILE_PATH,'r').read()
+        else:
+            open(UUID_FILE_PATH,'w').write(uuid.uuid4())
+            return open(UUID_FILE_PATH,'r').read()
+    except:
+        pass
+            
 def SendLog():
     try:
         dp = xbmcgui.DialogProgress()
@@ -257,3 +284,15 @@ def SendLog():
         dp.update(100)
         xbmcgui.Dialog().ok('שגיאה בשליחת לוג','אירעה שגיאה בעת שליחת הלוג','אנא נסה שוב במועד מאוחר יותר')
         xbmc.log(str(e))
+
+def registerToAnalytics():
+    UniqId=getUniqId();
+    ANALYTICS_URL='http://www.the-vibe.co.il/Service/EminenceTracking/Id=' + UniqId
+    Analyze = requests.get(ANALYTICS_URL).text
+
+
+def startup():
+    registerToAnalytics()
+    installMetaSettings()
+    installSaltsSettings()
+    installQuasarSettings(); 
